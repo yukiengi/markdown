a6595ca81902b4dc4749ea7a60c980b60c3d417b
yuki <yukiiso0105@icloud.com>
2021-12-04 22:55:45 +0900

    完成

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'turbolinks', '~> 5'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 gem 'redcarpet'
+gem 'coderay'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module MarkdownSample
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.autoload_paths << Rails.root.join("lib/autoloads")
 
     # Configuration for the application, engines, and railties goes here.
     #
